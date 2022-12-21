2d5e5d04be777f42b35667d1044dd47936cfd536
Dukevarun <W9218308@live.tees.ac.uk>
2022-12-21 11:03:05 +0000

    Added Algorithm for the task given
    Commented steps to complete the task of building the Simple Slot Machine

--- SlotMachine/SlotMachine/Program.cs
+++ SlotMachine/SlotMachine/Program.cs
@@ -1,5 +1,16 @@
 ﻿using System;
 
+//read deposit amount from user
+//read stake amount from user for every spin
+//create slot table
+//create new list of lists (table)
+// fill each list/row with 3 values
+//assign symbols
+//display the table
+//calculate winning coeffs for current spin
+//calculate win amount
+//update balance with the winning amount and display it
+
 namespace SlotMachine
 {
     class Program
